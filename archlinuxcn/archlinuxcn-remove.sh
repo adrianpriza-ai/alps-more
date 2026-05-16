4a4b7d6b7daf52f9f701e8d27de21cbd95e11472
adrianpriza-ai <adrianpriza18@gmail.com>
2026-05-16 10:48:57 +0700

    update

--- archlinuxcn/archlinuxcn-remove.sh
+++ archlinuxcn/archlinuxcn-remove.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
+
 set -e
-echo "[+] Removing archlinuxcn..."
+
+echo "[+] Removing archlinuxcn repository..."
+
 sudo sed -i '/^\[archlinuxcn\]/,+1d' /etc/pacman.conf
+
 sudo pacman -Rns --noconfirm archlinuxcn-keyring || true
+
+sudo pacman -Sy
+
 echo
 echo "[+] archlinuxcn removed."
--- chaotic-aur/chaotic-aur-remove.sh
+++ chaotic-aur/chaotic-aur-remove.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
+
 set -e
-echo "[+] Removing Chaotic-AUR..."
+
+echo "[+] Removing Chaotic-AUR repository..."
+
 sudo sed -i '/^\[chaotic-aur\]/,+1d' /etc/pacman.conf
+
 sudo pacman -Rns --noconfirm \
     chaotic-keyring \
     chaotic-mirrorlist || true
+
+sudo pacman -Sy
+
 echo
 echo "[+] Chaotic-AUR removed."
